c7f658da421206bb67cd072c233f88f94d0177be
eumyang99 <eumyang99@gmail.com>
2024-02-19 21:46:47 +0900

    #8 fix: 수정 - sidebar item 렌더링을 map 메소드로 수정

--- components/sidebar/sidebar-main.tsx
+++ components/sidebar/sidebar-main.tsx
@@ -1,63 +1,43 @@
 import { Text, View, TouchableOpacity, Image } from "react-native";
 import styles from "./style";
+
+const icons = {
+  search: require("../../assets/icon/search.png"),
+  wordseed: require("../../assets/icon/wordseed.png"),
+  follow: require("../../assets/icon/follow.png"),
+  alram: require("../../assets/icon/alram.png"),
+  settings: require("../../assets/icon/settings.png"),
+};
+
+const menus = {
+  search: "검색",
+  wordseed: "말씨",
+  follow: "관심작가",
+  alram: "알람",
+  settings: "설정",
+};
+
 export default function SidebarMain({ webViewRef }) {
   const pressHandler = (path: string) => {
+    console.log(path);
     webViewRef.current.postMessage({ data: path });
   };
 
   return (
     <View style={styles.main}>
       <View style={styles.contentWrapper}>
-        <TouchableOpacity
-          style={styles.content}
-          onPress={() => pressHandler("/search")}
-        >
-          <Image
-            style={styles.icon}
-            source={require("../../assets/icon/search.png")}
-          />
-          <Text style={styles.describe}>검색</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.content}
-          onPress={() => pressHandler("/wordseed")}
-        >
-          <Image
-            style={styles.icon}
-            source={require("../../assets/icon/lightbulb.png")}
-          />
-          <Text style={styles.describe}>말씨</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.content}
-          onPress={() => pressHandler("/follow/1")}
-        >
-          <Image
-            style={styles.icon}
-            source={require("../../assets/icon/artist.png")}
-          />
-          <Text style={styles.describe}>관심작가</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.content}
-          onPress={() => pressHandler("/alram")}
-        >
-          <Image
-            style={styles.icon}
-            source={require("../../assets/icon/notification.png")}
-          />
-          <Text style={styles.describe}>알람</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.content}
-          onPress={() => pressHandler("/settings")}
-        >
-          <Image
-            style={styles.icon}
-            source={require("../../assets/icon/settings.png")}
-          />
-          <Text style={styles.describe}>설정</Text>
-        </TouchableOpacity>
+        {Object.keys(menus).map((menu) => {
+          return (
+            <TouchableOpacity
+              key={menu}
+              style={styles.content}
+              onPress={() => pressHandler(`/${menu}`)}
+            >
+              <Image style={styles.icon} source={icons[menu]} />
+              <Text style={styles.describe}>{menus[menu]}</Text>
+            </TouchableOpacity>
+          );
+        })}
       </View>
     </View>
   );
